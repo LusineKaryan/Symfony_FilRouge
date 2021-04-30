89177697b4faabfdde2f4bfaa3ea8a340756968d
LusineKaryan <72447443+LusineKaryan@users.noreply.github.com>
2021-04-30 10:11:58 +0200

    commit mailer

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Form\ContactType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,10 +13,38 @@ class ContactController extends AbstractController
     /**
      * @Route("/contact", name="contact")
      */
-    public function index(): Response
+    public function index(Request $request, \Swift_Mailer $mailer): Response
     {
-       $form = $this->createForm(ContactType::class);
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
 
+        if($form->isSubmitted() && $form->isValid()){
+            $contact = $form->getData();
+
+            // Ici nous enverrons le mail
+            $message = (new \Swift_Message('Nouveau contact'))
+
+                // On attribue l'expéditeur
+                ->setFrom($contact['from'])
+
+                // On attribue le destinataire
+                ->setTo('contact@adresse.fr')
+
+                // On crée le message avec la vue Twig
+                ->setBody(
+                    $this->renderView(
+                        'emails/contact.html.twig', compact('contact')
+                    ),
+                    'text/html'
+                )
+            ;
+
+            // On envoi le message
+            $mailer->send($message);
+
+            $this->addFlash('message', 'Votre message a été envoyé.'); // Un message flash de renvoi
+            return $this->redirectToRoute('home');
+        }
         return $this->render('contact/index.html.twig', [
             'contact_form' =>$form->createView()
         ]);
